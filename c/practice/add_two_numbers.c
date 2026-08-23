0547f5112065fc6e88b85e70df7954881ca437c4
nthxdev <nthxlab@gmail.com>
2026-08-23 12:08:54 +0000

    initial commit

--- c/practice/add_two_numbers.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-int main(){
-    int a,b,sum=0;
-    printf("Enter numbers: ");
-    scanf("%d %d",&a,&b);
-    sum=a+b;
-    printf("sum of %d + %d is %d\n",a,b,sum);
-}
--- java/practice/area_of_circle.java
+++
@@ -1,6 +0,0 @@
-import java.util.Scanner;
-class area_of_circle{
-    public static void main(String[] args){
-        
-    }
-}
